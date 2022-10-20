8477e75496a96f03b56dabcd24aa62dd4f3df43f
Tevye Krynski <tkrynski@gmail.com>
2022-10-19 18:15:53 -0700

    Update google_map_place_picker.dart

--- lib/src/google_map_place_picker.dart
+++ lib/src/google_map_place_picker.dart
@@ -335,15 +335,16 @@ class GoogleMapPlacePicker extends StatelessWidget {
             textAlign: TextAlign.center,
           ),
           SizedBox(height: 10),
-          RaisedButton(
-            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          // RaiseButton(
+          ElevatedButton(
+//            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
             child: Text(
               "Select here",
               style: TextStyle(fontSize: 16),
             ),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(4.0),
-            ),
+//            shape: RoundedRectangleBorder(
+//              borderRadius: BorderRadius.circular(4.0),
+//            ),
             onPressed: () {
               onPlacePicked!(result);
             },
